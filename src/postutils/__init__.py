f28f10ba3a6082a19e01d141cae36c730e8a4530
Jakob Schreiner <jakob2602@gmail.com>
2018-05-14 14:53:31 +0200

    preprocess cellmodel now works on datamap, key rather than any explicit dict or np.record

--- src/postutils/__init__.py
+++ src/postutils/__init__.py
@@ -10,3 +10,5 @@ from .configs import (
 )
 
 from .cellmodel_preprocessing import preprocess_wei
+
+from .wei_utils import wei_uniform_ic
--- src/postutils/cellmodel_preprocessing.py
+++ src/postutils/cellmodel_preprocessing.py
@@ -11,8 +11,10 @@ from collections import (
 from typing import (
     Dict,
     Tuple,
+    List,
     Any,
     Generator,
+    Mapping,
 )
 
 from postspec import PlotSpec
@@ -21,23 +23,25 @@ from postspec import PlotSpec
 LOGGER = logging.getLogger(name=__name__)
 
 
-def preprocess_wei_variable(
-        structured_array: np.ndarray,
+def preprocess_wei(
+        data_map: Mapping[str, np.ndarray],
+        names: List[str],
         params: Dict[str, float],
         kwargs: Dict[str, Any]
-) -> Generator[Tuple[np.ndarray, PlotSpec]]:
+) -> Generator[Tuple[np.ndarray, PlotSpec], None, None]:
     """
     Scale the inputs so they are appropriate to plot.
 
     Args:
-        structured_array: An array with the names of the parameters as dtype.
+        data_map: A map from a nme to an array.
+        names: The keys in `data_map`.
         params: The parameters `vol` and `beta0` from the solver.
         kwargs: `PotSpec` keyword arguments.
 
     Return an iterator of the scaled data and corresponding plot spec.
     """
     msg = "Need the volume to compute skalings for all the ions,"""
-    assert "voli" in structured_array.dtype, msg
+    assert "Voli" in names, msg
 
     # TODO: Move this to some global place?
     # NB! This relies on built-in dicts being ordered
@@ -46,13 +50,13 @@ def preprocess_wei_variable(
         "m": (r"Voltage Gate (m)", "mV"),
         "h": (r"Voltage Gate (n)", "mV"),
         "n": (r"Voltage Gate (h)", "mV"),
-        "Ko": (r"Extracellular Potassium $[K^+]$", "mol"),
-        "Ki": (r"Intracellular Potessium $[K^+]$", "mol"),
-        "Nao": (r"Extracellular Sodium $[Na^+]$", "mol"),
-        "Nai": (r"Intracellular Sodium $[Na^+]$", "mol"),
-        "Clo": (r"Exctracellular Chlorine $[Cl^-]$", "mol"),
-        "Cli": (r"Intracellular Chlorine $[CL^-]$", "mol"),
-        "beta": (
+        "NKo": (r"Extracellular Potassium $[K^+]$", "mol"),
+        "NKi": (r"Intracellular Potessium $[K^+]$", "mol"),
+        "NNao": (r"Extracellular Sodium $[Na^+]$", "mol"),
+        "NNai": (r"Intracellular Sodium $[Na^+]$", "mol"),
+        "NClo": (r"Exctracellular Chlorine $[Cl^-]$", "mol"),
+        "NCli": (r"Intracellular Chlorine $[CL^-]$", "mol"),
+        "Voli": (
             r"Ratio of intracellular to extracellular volume",
             r"$Vol_i/Vol_e$"
         ),
@@ -61,11 +65,11 @@ def preprocess_wei_variable(
 
     vol = params["vol"]         # Initial colume
     beta0 = params["beta0"]     # Initial volume ratio
-    voli = structured_array["voli"]
+    voli = data_map["Voli"]
     volo = (1 + 1/beta0)*vol * voli
 
-    for name, _ in structured_array.dtype:
-        data = structured_array[name]
+    for name in names:
+        data = data_map[name]
 
         if name == "NKo":
             data /= volo
@@ -89,4 +93,4 @@ def preprocess_wei_variable(
             "xlabel": variable_dict[name][0]
         }
         plot_spec_kwargs.update(kwargs)
-        yield values[:, i], PlotSpec(**plot_spec_kwargs)
+        yield data, PlotSpec(**plot_spec_kwargs)
